42b5ca92efedc556ee8f4c8c312b5561e23e6d6a
jessmelo <jessmelo@usp.br>
2024-04-26 18:23:12 -0300

    starting with pivot

--- src/algorithms/QuickSort.java
+++ src/algorithms/QuickSort.java
@@ -2,6 +2,13 @@ package algorithms;
 
 public class QuickSort {
     public static void sort(int[] arr) {
-        // TODO: Implement QuickSort
+        // get initial pivot element
+        int pivot = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < pivot) {
+                int temp = arr[i];
+                
+            }
+        }
     }
 }
